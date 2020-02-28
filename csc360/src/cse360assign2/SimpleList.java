e787076d54ae40c4f507afb9ccb31b5f80f6db9f
w b <w.barnett2426@gmail.com>
2020-02-27 19:28:34 -0700

    finished part 2

--- csc360/src/cse360assign2/SimpleList.java
+++ csc360/src/cse360assign2/SimpleList.java
@@ -12,14 +12,24 @@ class SimpleList {
 	
 	SimpleList()
 	{
-		list =new int[10];	// allocates space for ten integers
+		list = new int[10];	// allocates space for ten integers
 		count = 0;
-	} 
+	}
 
 	void add(int x) 
 	{		
-		// temporary holder for list
-		int[] temp = new int[10];
+		count++;
+		int[] temp;
+		
+		if(count > list.length)
+		{
+			int newSize = list.length + (list.length /2);
+			temp = new int[newSize];
+		}
+		else
+		{
+			temp = new int[10];
+		}
 		
 		// adds element to the list
 		temp[0] = x;
@@ -32,11 +42,52 @@ class SimpleList {
 		
 		// set list to new updated list
 		list = temp;
+	}
+	
+	// removing an element from the list
+	void remove(int x)
+	{
+		// indexing 
+		int index = 0;
 		
-		// if list is less than 10 elements, up the count
-		if(count < 10)
+		//starts the search
+		while(index <= count)
 		{
-			count++;
+			// comparing
+			if(x == list[index])
+			{
+				// removes the element by replacing it with the element after
+				while(index < count)
+				{
+					list[index] = list[index + 1];
+					index++;
+				}
+				// no repeats of the last two elements
+				list[index] = (Integer) null;
+			}
+			// move onto next index
+			index++;
+		}
+		// brings down count
+		count--;
+		
+		// if less than 25% empty, using double for decimal
+		double percent = count / list.length;
+		
+		if (percent <= 0.25)
+		{
+			// floor of new size of list
+			int newSize = list.length / 2;
+			
+			// temp list with new size
+			int[] temp = new int[newSize];
+			
+			for(int i = 0; i <= count; i++)
+			{
+				temp[i] = list[i];
+			}
+			
+			list = temp;
 		}
 	}
 	
@@ -82,19 +133,7 @@ class SimpleList {
 		}
 		
 		// returns -1 if the paramter was not found
-		return -1;
-		
+		return -1;	
 	}
 	
-	// returns first integer
-	int first()
-	{
-		return list[0];
-	}
-	
-	// returns last integer
-	int last()
-	{
-		return list[count];
-	}
 }
